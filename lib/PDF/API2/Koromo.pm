80be8a0d2744c4836ec37c080e521ae048164347
issm <issmxx@gmail.com>
2011-05-16 00:54:31 +0900

    initial commit

---
+++ lib/PDF/API2/Koromo.pm
@@ -0,0 +1,32 @@
+package PDF::API2::Koromo;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+PDF::API2::Koromo -
+
+=head1 SYNOPSIS
+
+  use PDF::API2::Koromo;
+
+=head1 DESCRIPTION
+
+PDF::API2::Koromo is
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
